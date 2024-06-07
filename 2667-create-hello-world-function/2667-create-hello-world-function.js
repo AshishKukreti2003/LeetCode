d398c9ea2afdd3bab3c284c5cc204762c82e539f
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-06-07 23:57:42 +0530

    Time: 49 ms (64.17%), Space: 48.8 MB (36.66%) - LeetHub

---
+++ 2667-create-hello-world-function/2667-create-hello-world-function.js
@@ -0,0 +1,14 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function() {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
