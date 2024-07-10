80d7200b032b30dc881236ae62b51f40e1f9ae25
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-10 21:32:24 +0530

    Time: 99 ms (38.62%), Space: 49.9 MB (60.31%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const result = [];
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j] === target){
+                result.push(i,j);
+                break;
+            }
+        }
+    }
+    return result;
+};
