42082233f33dc35052b99548120bfca29541c5f6
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-17 23:39:26 +0530

    Time: 75 ms (21.76%), Space: 54.1 MB (23.09%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    const arr = [];
+    for(let i=0;i<nums.length;i++){
+        arr.push(nums[nums[i]]);
+    }
+    return arr;
+};
