72c9b9e6e7eee9d0f00c6776566620cf85f4f501
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-17 23:51:52 +0530

    Time: 59 ms (50.09%), Space: 51.3 MB (34.76%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function(nums) {
+    let sum = 0;
+    const arr = [];
+    for(let i=0;i<nums.length;i++){
+        sum+=nums[i];
+        arr.push(sum);
+    }
+    return arr;
+};
