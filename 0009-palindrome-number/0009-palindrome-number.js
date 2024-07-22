f7511a3a0f78a1a9f89600e972e067c849d59598
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-22 16:35:53 +0530

    Time: 118 ms (88.65%), Space: 56.5 MB (85.95%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let n = x, sum = 0, rem = 0;
+    while(n>0){
+        rem = n%10;
+        sum = sum*10+rem;
+        n = Math.floor(n/10);
+    }
+    return sum == x;
+};
