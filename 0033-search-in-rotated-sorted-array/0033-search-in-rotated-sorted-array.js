491538b4b14a805ac5fe4b513658615935f8978c
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-25 01:18:49 +0530

    Time: 52 ms (64.08%), Space: 49 MB (54.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,35 +4,17 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let pivot = Pivot(nums);
-    if(pivot === 0 || target<nums[0]){
-        return BinarySearch(nums,target,pivot,nums.length-1);
-    }else{
-        return BinarySearch(nums,target,0,pivot-1);
-    }
+    return BinarySearch(nums,target,0,nums.length-1);
     
-    function BinarySearch(nums, target,s,e){
-        while(s<=e){
-            let mid = Math.floor((s+e)/2);
-            if(nums[mid] === target) return mid;
-            else if(nums[mid] > target) e=mid-1;
-            else s=mid+1;
+    function BinarySearch(nums,target,s,e){
+        if(s>e) return -1;
+        let mid = Math.floor(s+(e-s)/2);
+        if(nums[mid] === target) return mid;
+        if(nums[s] <= nums[mid]){
+            if(nums[s] <= target && target<=nums[mid]) return BinarySearch(nums,target,s,mid-1);
+            else return BinarySearch(nums,target,mid+1,e);
         }
-        return -1;
-    }
-    
-    function Pivot(nums){
-        let s = 0,end = nums.length-1;
-        while(s<end){
-            let mid = Math.floor((s+end)/2);
-            if(nums[mid]>nums[mid+1]){
-                return mid+1;
-            }else if(nums[mid] >= nums[s]){
-                s=mid+1;
-            }else{
-                end= mid;
-            }
-        }
-        return 0;
+        if(target>= nums[mid] && target<= nums[e]) return BinarySearch(nums,target,mid+1,e);
+        else return BinarySearch(nums,target,s,mid-1);
     }
 };
