468f667c0524c5337584b3b72519fa1af96cede0
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-25 16:12:48 +0530

    Time: 277 ms (13.88%), Space: 57.8 MB (8.68%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,38 +1,25 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        if(nums.length == 1){
-            return nums;
-        }
-        int mid = nums.length/2;
-        int[] left = sortArray(Arrays.copyOfRange(nums,0,mid));
-        int[] right = sortArray(Arrays.copyOfRange(nums,mid,nums.length));
-        
-        return merge(left,right);
+        quick_sort(nums,0,nums.length-1);
+        return nums;
     }
     
-    public static int[] merge(int[] first, int[] second){
-        int[] mix = new int[first.length+second.length];
+    static void quick_sort(int[] arr, int low, int high){
+        if(low>=high) return;
         
-        int i=0,j=0,k=0;
-        while(i<first.length && j<second.length){
-            if(first[i] < second[j]){
-                mix[k] = first[i];
-                i++;
-            }else{
-                mix[k] = second[j];
-                j++;
-            }k++;
+        int s=low,e=high,m=s+(e-s)/2,pivot=arr[m];
+        while(s<=e){
+            while(arr[s]<pivot) s++;
+            while(arr[e]>pivot) e--;
+            if(s<=e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
         }
-        while(i<first.length){
-            mix[k] = first[i];
-            i++;
-            k++;
-        }
-        while(j<second.length){
-            mix[k] = second[j];
-            j++;
-            k++;
-        }
-        return mix;
+        quick_sort(arr,low,e);
+        quick_sort(arr,s,high);
     }
 }
