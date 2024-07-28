0fa54d8ef0899f52db5ce6f815518a1ae70c76b7
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-29 00:29:58 +0530

    Time: 9 ms (92.89%), Space: 56.7 MB (50.12%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left,right);
+    }
+    
+    public ListNode merge(ListNode left, ListNode right){
+        ListNode ans = new ListNode();
+        ListNode temp = ans;
+        while(left!=null && right!=null){
+            if(left.val < right.val){
+                temp.next = left;
+                left = left.next;
+                temp = temp.next;
+            }else{
+                temp.next = right;
+                right = right.next;
+                temp = temp.next;
+            }
+        }
+        temp.next = (left!=null) ? left : right;
+        return ans.next;
+    }
+    
+    public ListNode getMid(ListNode head){
+        ListNode midPrev = null;
+        while(head!=null && head.next!=null){
+            midPrev = (midPrev == null) ? head : midPrev.next;
+            head = head.next.next;
+        }
+        ListNode mid = midPrev.next;
+        midPrev.next = null;
+        return mid;
+    }
+}
