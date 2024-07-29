092e77f2b556d25e331a9587e2fb8da283e3fdaf
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-29 16:05:44 +0530

    Time: 57 ms (36.15%), Space: 49 MB (30.05%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function(num) {
+    let count = 0;
+    while(num !=0){
+        if(num%2 == 0){
+        num/=2;
+        count++;
+        }else{
+            num-=1
+            count++;
+        }
+    }
+    return count;
+};
