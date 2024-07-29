97025171b9a7694cb4852ae24de170e2c438f7e5
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-07-29 16:02:09 +0530

    Time: 43 ms (92.33%), Space: 48.6 MB (74.81%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    const newS = s.trim();
-    let count = 0;
-    for(let i=newS.length-1;i>=0;i--){
-        if(newS.charAt(i) != ' '){
-            count++;
-        }else{
-            break;
-        }
-    }
-    return count;
+    const words = s.trim().split(' ');
+    return words.length > 0 ? words[words.length-1].length : 0;
 };
