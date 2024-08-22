7dcbbdb29cee341d671cb017395f96fb93c7941c
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-08-22 23:10:26 +0530

    Time: 70 ms (98.82%), Space: 55.9 MB (23.03%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -6,14 +6,12 @@
 var addToArrayForm = function(num, k) {
     let arr = [];
     let i = num.length-1;
-    while(i>= 0 || k> 0){
+    while(i>=0 || k>0){
         if(i>=0){
-            arr.push((num[i]+k)%10);
-            k = Math.floor((num[i]+k)/10);
-        }else{
-            arr.push(k%10);
-            k = Math.floor(k/10);
+            k+=num[i];
         }
+        arr.push(k%10);
+        k = Math.floor(k/10);
         i--;
     }
     arr.reverse();
