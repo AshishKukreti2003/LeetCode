c807f649c7fe4b2000f473bf60a427c28385c394
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-08-24 22:57:35 +0530

    Time: 42 ms (97.02%), Space: 49 MB (82.54%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.js
@@ -4,8 +4,27 @@
  * @return {boolean}
  */
 var search = function(nums, target) {
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] === target) return true;
+    let left = 0, right = nums.length-1;
+    while(left<=right){
+        let mid = Math.floor(left + (right-left)/2);
+        if(nums[mid] === target) 
+            return true;
+        if(nums[left] === nums[mid] && nums[mid] === nums[right]){
+            left++;
+            right--;
+        }else if(nums[left] <= nums[mid]){
+            if(nums[left] <= target && target <= nums[mid]){
+                right = mid-1;
+            }else{
+                left = mid+1;
+            }
+        }else{
+            if(nums[right] >= target && target >= nums[mid]){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
     }
     return false;
 };
