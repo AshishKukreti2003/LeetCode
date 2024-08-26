0a318e062b396b2d7028aad3732fe2795e4c182c
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-08-27 00:39:14 +0530

    Time: 235 ms (28.27%), Space: 62.1 MB (91.79%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.js
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -3,48 +3,11 @@
  * @return {number}
  */
 var maximumGap = function(nums) {
-    if(nums.length < 2)
-        return 0;
-    
-    nums = sort(nums);
+    if(nums.length < 2) return 0;
+    nums.sort((a,b) => a-b);
     let maxGap = 0;
     for(let i=1;i<nums.length;i++){
-        maxGap = Math.max(maxGap,nums[i] - nums[i-1]);
+        maxGap = Math.max(maxGap, nums[i] - nums[i-1]);
     }
     return maxGap;
 };
-
-function sort(nums){
-    if (nums.length <= 1) {
-        return nums;
-    }
-    let mid = Math.floor(nums.length/2);
-        let left = sort(nums.slice(0,mid));
-        let right = sort(nums.slice(mid,nums.length));
-        
-        return merge(left,right);
-        
-    }
-    
-function merge(left,right){
-        let mix = [];
-        let i=0,j=0;
-        while(i<left.length && j<right.length){
-            if(left[i] < right[j]){
-                mix.push(left[i]);
-                i++;
-            }else{
-                mix.push(right[j]);
-                j++;
-            }
-        }
-        while(i<left.length){
-            mix.push(left[i]);
-            i++;
-        }
-        while(j<right.length){
-            mix.push(right[j]);
-            j++;
-        }
-    return mix;
-    }
