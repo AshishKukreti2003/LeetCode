ae896bf4cebcb8ed3fea6648e306fb5d0a098c8c
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-08-28 00:19:56 +0530

    Time: 50 ms (90.00%), Space: 49.6 MB (56.67%) - LeetHub

---
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @param {number} d
+ * @return {number}
+ */
+var findTheDistanceValue = function(arr1, arr2, d) {
+    let count = 0;
+    for(let i=0;i<arr1.length;i++){
+        for(let j=0;j<arr2.length;j++){
+            if(Math.abs(arr1[i] - arr2[j]) <= d){
+                count++;
+                break;
+            }
+        }
+    }
+    return arr1.length-count;
+};
