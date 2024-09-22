b92b1b590fb80c66d9df4e7c2e4183527d196f7e
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-09-22 09:55:13 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (33.18%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,19 +1,19 @@
 class Solution {
     public String interpret(String command) {
-        StringBuilder s = new StringBuilder();
-        int i=0;
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
         while(i<command.length()){
             if(command.charAt(i) == 'G'){
-                s.append('G');
+                sb.append(command.charAt(i));
                 i++;
-            }else if(command.charAt(i) == '(' && command.charAt(i+1) ==')'){
-                s.append('o');
+            }else if(command.charAt(i) == '(' && command.charAt(i+1) == ')'){
+                sb.append('o');
                 i+=2;
             }else{
-                s.append("al");
+                sb.append("al");
                 i+=4;
             }
         }
-        return s.toString();
+        return sb.toString();
     }
 }
