0dc5cc7652f69cc989190cc1ed6eb0d98c0b97cb
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-10-11 21:33:27 +0530

    Time: 0 ms (100.00%), Space: 40 MB (82.49%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        if(num ==1){
-            return true;
-        }
-        for(int i=1;i<=Math.sqrt(num);i++){
-            if(Math.sqrt(num) == i){
+        if(num == 1) return true;
+        int start = 2, end = num/2;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            long square = (long)mid*mid;
+            if(square == num){
                 return true;
-            }
+            }else if(square > num){
+                end = mid -1;
+            }else start = mid+1;
         }
         return false;
     }
