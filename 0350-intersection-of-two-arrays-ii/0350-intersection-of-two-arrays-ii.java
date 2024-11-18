6a8390953eb515f2e0ad0399455bd08afdb83069
Ashish Kukreti <123192221+AshishKukreti2003@users.noreply.github.com>
2024-11-18 21:40:33 +0530

    Time: 2 ms (95.13%), Space: 44.5 MB (5.17%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        nums1 = mergeSort(nums1);
-        nums2 = mergeSort(nums2);
+        nums1 = merge(nums1);
+        nums2 = merge(nums2);
         int i=0,j=0;
         ArrayList<Integer> list = new ArrayList<>();
         while(i<nums1.length && j<nums2.length){
             if(nums1[i] == nums2[j]){
-                list.add(nums1[i]);
+                list.add(nums2[j]);
                 i++;
                 j++;
             }else if(nums1[i] > nums2[j]){
@@ -17,45 +17,39 @@ class Solution {
         }
         
         int[] arr = new int[list.size()];
-        int o=0;
+        i=0;
         for(int num: list){
-            arr[o++] = num;
+            arr[i++] = num;
         }
         return arr;
     }
     
-    public static int[] mergeSort(int[] arr){
+    private static int[] merge(int[] arr){
         if(arr.length == 1) return arr;
         int mid = arr.length/2;
-        int[] left = mergeSort(Arrays.copyOfRange(arr,0,mid));
-        int[] right = mergeSort(Arrays.copyOfRange(arr,mid,arr.length));
+        int[] left = merge(Arrays.copyOfRange(arr,0,mid));
+        int[] right = merge(Arrays.copyOfRange(arr,mid,arr.length));
         
-        return merge(left,right);
+        return mergeSort(left,right);
     }
     
-    public static int[] merge(int[] left, int[] right){
+    private static int[] mergeSort(int[] left, int[] right){
         int[] main = new int[left.length+right.length];
         int i=0,j=0,k=0;
         while(i<left.length && j<right.length){
             if(left[i] < right[j]){
-                main[k] = left[i];
-                i++;
+                main[k++] = left[i++];
             }else{
-                main[k] = right[j];
-                j++;
+                main[k++] = right[j++];
             }
-            k++;
         }
         
         while(i<left.length){
-            main[k] = left[i];
-            k++;
-            i++;
+            main[k++] = left[i++];
         }
+        
         while(j<right.length){
-            main[k] = right[j];
-            j++;
-            k++;
+            main[k++] = right[j++];
         }
         
         return main;
